c0e33ea1099d5742fcc6ec90d42da58f18c37ba4
Rainer <abelass@Chuperlapup.(none)>
2012-10-29 07:34:47 +0100

    import

---
+++ lang/paquet-mcp_agenda_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+	// M
+	'mcp_agenda_description' => 'Extension Agenda pour le pack My Chacra Démarrage',
+	'mcp_agenda_nom' => 'My Chacra - Extension Agenda',
+	'mcp_agenda_slogan' => 'Un agenda événementiel pour mon site',
+);
+
+?>
